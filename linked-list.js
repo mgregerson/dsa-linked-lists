14d2b2378da354c2e7a94a3635ca06019e41faa6
mgregerson <mgregerson4321@gmail.com>
2023-05-29 15:31:04 -0400

    Completed push and unshift

--- linked-list.js
+++ linked-list.js
@@ -20,22 +20,55 @@ class LinkedList {
     for (let val of vals) this.push(val);
   }
 
+  // bee is current 
+    // if current !== null, make current current.next;
+  // vals = ["bee", "ant", "caterpillar", "mosquito"]
+
   /** push(val): add new value to end of list. */
 
   push(val) {
+    let newNode = new Node(val);
+    this.length += 1;
 
+    if (this.head === null) this.head = newNode;
+    
+    if (this.tail !== null) this.tail.next = newNode;
+
+    this.tail = newNode;
   }
 
   /** unshift(val): add new value to start of list. */
 
   unshift(val) {
-
+    let newNode = new Node(val);
+    newNode.next = this.head;
+    this.head = newNode;
+    if (this.length === 0) this.tail = newNode;
+    this.length += 1;
   }
 
   /** pop(): return & remove last item. */
 
   pop() {
-
+    let response = this.tail;
+    let current = this.head;
+    if (current === null) {
+      throw new Error('This list is empty!')
+    }
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    }
+    while (current.next !== null) {
+      if (current.next !== this.tail) {
+        current = current.next;
+      }
+      break;
+    }
+    this.tail = current;
+    this.tail.next = null;
+    this.length -= 1;
+    return response.val;
   }
 
   /** shift(): return & remove first item. */
@@ -74,5 +107,8 @@ class LinkedList {
     
   }
 }
+
+// let newlist = new LinkedList([new Node('ant')])
+// console.log(newlist.pop())
 
 module.exports = LinkedList;
